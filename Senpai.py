be3fb1c0f72a9cab2a84064ec9e4cc926120bf98
phoomct <57429408+phoomct@users.noreply.github.com>
2020-03-16 18:27:56 +0700

    Update Senpai.py

--- Senpai.py
+++ Senpai.py
@@ -1,18 +1,23 @@
-""" bot """
 import discord
 from discord.ext import commands
-bot = commands.Bot(command_prefix='<help>')
+tokken = 'Njg4Nzc1NDQ4Mzk3NzQyMjEy.Xm8YAw.sTztDYkSkkBXcUoP36BR5txJr04'
+bot = commands.Bot(command_prefix='.')
 @bot.event
-async def on_ready():
-    print("Sorry for my late")
+async def on_ready() :
+    print("Sorry for my late oMo")
 
 @bot.event
-async def on_message(message):
-    if message.content.startswith('Hello'):
-        await message.channel.send('hi ><')
-    elif message.content.startswith('Senpai'):
-        await message.channel.send('nani nani oWo')
+async def on_member_join(member):
+    print(f'{member} has join the server.')
 
-#@bot.event
+@bot.event
+async def on_member_remove(member):
+    print(f'{member} has left the server.')
+     return await message.channel.send(f"{member} has left the server")
 
-bot.run('Njg4Nzc1NDQ4Mzk3NzQyMjEy.Xm7xeQ.-5MeGmGKfzFIKoX8w3jjkZfxN5s')
+
+@bot.event
+async def on_message(message) :
+    if message.content.startswith ('Hello'):
+       return await message.channel.send('hi ~ Meow ><')
+bot.run(tokken)
