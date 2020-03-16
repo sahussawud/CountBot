3c343e5ea3d79b08084708de4b5a1030847c6053
phoomct <57429408+phoomct@users.noreply.github.com>
2020-03-16 20:27:10 +0700

    Update Senpai.py
    Bot Join Discord

--- Senpai.py
+++ Senpai.py
@@ -1,25 +1,16 @@
 import discord
 from discord.ext import commands
+
 tokken = 'Njg4Nzc1NDQ4Mzk3NzQyMjEy.Xm8YAw.sTztDYkSkkBXcUoP36BR5txJr04'
-bot = commands.Bot(command_prefix='.')
-@bot.event
+client = commands.Bot(command_prefix='.')
+
+@client.event
 async def on_ready() :
-    print("Sorry for my late oMo")
+    print("Bot online.")
 
-@bot.event
-async def on_member_join(member):
-    print(f'{member} has join the server.')
+@client.command(pass_context=True)
+async def join(ctx):
+    channel = ctx.message.author.voice.channel
+    await channel.connect()
 
-@bot.event
-async def on_member_remove(member):
-    print(f'{member} has left the server.')
-
-@bot.event
-async def on_message(message) :
-    if message.content.startswith ('Hello'):
-       return await message.channel.send('hi ~ Meow ><')
-
-@bot.command()
-async def ping(word):
-    await word.send('Pong!')
-bot.run(tokken)
+client.run(tokken)
